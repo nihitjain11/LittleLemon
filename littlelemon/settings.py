bed18ca449077ef51abddb2e4d4a2236e13cc05a
Nihit Jain <nihitjain11@gmail.com>
2024-09-28 10:28:18 +0530

    add static routes and files

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'littlelemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# STATIC_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
