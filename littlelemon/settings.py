a575d5ff2ad6cd89950e9b3c70f74ebc96062afd
Nihit Jain <nihitjain11@gmail.com>
2024-09-28 15:15:23 +0530

    add MySQL support

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -75,9 +75,20 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'root',
+        'PASSWORD': 'root@123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
@@ -118,9 +129,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATIC_DIRS = [
+# STATICFILE_DIRS = [
 #     BASE_DIR / 'static'
 # ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
